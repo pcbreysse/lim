aa9889f0dc9e75615396996efa1064e8c50b6512
Jose Luis Bernal <jlbernal@Joses-MacBook-Pro.local>
2019-10-22 13:52:11 -0400

    new param set

--- params.py
+++ params.py
@@ -1,8 +1,17 @@
 import astropy.units as u
 
-default_par = dict(cosmo_model = 'Planck15',   # Input cosmological model
+default_par = dict(cosmo_input = dict(f_NL=0,H0=67.0,cosmomc_theta=None,ombh2=0.022, omch2=0.12, 
+                               omk=0.0, neutrino_hierarchy='degenerate', 
+                               num_massive_neutrinos=1, mnu=0.06, nnu=3.046, 
+                               YHe=None, meffsterile=0.0, standard_neutrino_neff=3.046, 
+                               TCMB=2.7255, tau=None, deltazrei=None, bbn_predictor=None, 
+                               theta_H0_range=[10, 100],w=-1.0, cs2=1.0, 
+                               dark_energy_model='ppf',As=2e-09, ns=0.96, nrun=0, 
+                               nrunrun=0.0, r=0.0, nt=None, ntrun=0.0, 
+                               pivot_scalar=0.05, pivot_tensor=0.05,
+                               parameterization=2,halofit_version='mead'),
               model_type = 'LF',    # Type of line model, either 'LF' or 'ML'
-              model_par = {'phistar':8.7e-11*u.Lsun**-1*u.Mpc**-3,
+              model_par = {'phistar':9.6e-11*u.Lsun**-1*u.Mpc**-3,
                            'Lstar':2.1e6*u.Lsun,
                            'alpha':-1.87,
                            'Lmin':5000*u.Lsun},    # Model parameters
@@ -11,19 +20,27 @@ default_par = dict(cosmo_model = 'Planck15',   # Input cosmological model
               Mmin = 1e9*u.Msun,    # Minimum mass for line emission
               Mmax = 1e15*u.Msun,    # Maximum mass for line emission
               nM = 5000,    # Number of points to compute mass functions
-              hmf_model = 'Tinker08',    # Mass function model. Choose from hmf
+              hmf_model = 'ST',    # Mass function model. Choose from hmf
+              bias_model = 'ST99',   # halo bias model
+              bias_par={}, #Otherwise, write a dict with the corresponding values
               Lmin = 10*u.Lsun,    # Minimum luminosity for dn/dL
               Lmax = 1e8*u.Lsun,    # Maximum luminosity for dn/dL
               nL = 5000,    # Number of points to compute luminosity functions
               kmin = 1e-2/u.Mpc,    # Minimum wavenumber for power spectra
-              kmax = 7/u.Mpc,    # Maximum wavenumber for power spectra
+              kmax = 10/u.Mpc,    # Maximum wavenumber for power spectra
               nk = 100,    # Number of wavenumber points for power spectra
+              k_kind = 'log',   #Kind of sampling in k
               sigma_scatter = 0.,    # Scatter in mass-luminosity relation
               fduty = 1.,    # Fraction of halos emitting at any given time
               do_onehalo = False,    # Bool to include one-halo contributions
               do_Jysr = False,    # Compute quantities in brightness temp if
                                  # False, Jy/sr if True
-                                 
+              do_RSD = True,    #Bool to include RSD contributions
+              sigma_NL = 7.*u.Mpc,   #Scale for non linear suppresion due to FoG
+              nmu = 1000,           #list of mu (cos theta) for integration or anisotropic
+              nonlinear_pm = False, #linear or non linear matter power spectrum                   
+              FoG_damp='Lorentzian', #Kind of damping for FoG
+              smooth=False, #Smoothed power spectrum (convoluted with beam/channel)
               # PARAMETERS USED FOR Line_Obs MODELS
               Tsys_NEFD = 40*u.K,    # System temperature
               Nfeeds = 19,    # Number of detectors
@@ -98,5 +115,4 @@ Silva_m2_z6_CCATp = dict(model_type = 'ML',
                          do_Jysr=True)
                          
               
-                                 
               
